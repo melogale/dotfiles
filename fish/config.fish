e0f806781b742a2ff84683c9844dbaf1d51df66b
melogale <zachariabadgers@gmail.com>
2025-11-03 14:49:19 -0500

    hi

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set fish_greeting ""
+set fish_greeting "Hello:"
 
 function fish_prompt
     string join '' \
